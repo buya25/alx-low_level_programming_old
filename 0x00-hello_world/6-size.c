d56137814553ecd32b9f5365d8fd8e12bb5d8c93
Yabs <yabsmullo0@gmail.com>
2023-03-16 03:42:14 -0700

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
