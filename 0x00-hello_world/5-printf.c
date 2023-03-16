602093b33302b27eecdc08c0e18ccd4185e546d0
Yabs <yabsmullo0@gmail.com>
2023-03-16 03:48:37 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  * main - prints the phrase with proper grammer,
  * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero
+ * Return: 0 if exited properly, non-zer otherwise
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
