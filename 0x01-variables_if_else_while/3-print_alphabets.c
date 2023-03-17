899bf2d64d5066bbe111b91fde5404ca5328db99
Yabs <yabsmullo0@gmail.com>
2023-03-17 05:56:32 -0700

    new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
-        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
+        int ch;
 
-        for (i = 0; i < 52; i++)
-        {
-                putchar(alp[i]);
-
-        }
-        putchar('\n');
-        return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
-
