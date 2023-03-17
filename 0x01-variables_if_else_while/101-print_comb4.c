89a240f713990d6253b8df55699b74ec7c3fda36
Yabs <yabsmullo0@gmail.com>
2023-03-17 06:53:54 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n <= 56; n++)
+	for (n = 48; n <= 58; n++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (m = 49; m <= 58; m++)
 		{
 			for (l = 50; l < 58; l++)
 			{
@@ -28,6 +28,6 @@ int main(void)
 			}
 		}
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
