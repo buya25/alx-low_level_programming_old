53efb48ee5461236c0684c8c6cd9d72a42dbce69
Yabs <yabsmullo0@gmail.com>
2023-04-05 02:43:15 -0700

    0x08. C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 	int sum = 0;
 
 	/**
-	 * if we reach the end of s1 and the char in s2 is a*
+	 * if we reach the end of s1 and the char in s2 is a *
 	 * and if the next chars of s2 are *, return 1
 	 */
 	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
