3dc81a1ec4962600455e486c77f772165349163c
Yabs <yabsmullo0@gmail.com>
2023-04-20 08:48:28 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -1,10 +1,10 @@
 #include "variadic_functions.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ ** main - check the code
+ **
+ ** Return: Always 0.
+ **/
 int main(void)
 {
 	    print_all("ceis", 'B', 3, "stSchool");
