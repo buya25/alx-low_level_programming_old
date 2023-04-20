6fa779c7a0532492fcc74bcd0201d2f7a34cf695
Yabs <yabsmullo0@gmail.com>
2023-04-20 08:38:55 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,11 @@
 #include "variadic_functions.h"
 
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
