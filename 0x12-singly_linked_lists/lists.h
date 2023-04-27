1018c9623800e652e6c2d4f1e283770fb46841db
Yabs Mullo <yabsmullo0@gmail.com>
2023-04-27 07:24:33 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,14 +8,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for the Holberton project
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-}list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
