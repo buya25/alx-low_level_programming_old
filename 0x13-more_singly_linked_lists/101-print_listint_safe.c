656e7c5a9675576447e4d02ec2c069d01db34622
Yabs Mullo <yabsmullo0@gmail.com>
2023-05-02 04:44:05 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,8 @@
 #include "lists.h"
+#include "lists.h"
 
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 /**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
@@ -26,4 +29,45 @@ size_t print_listint_safe(const listint_t *head)
 	}
 
 	return (num);
+}
+
+/**
+ * looped_listint_len - unique nodes are counted
+ * looped in listint_t
+ * @head: head of the listint_t to check.
+ *
+ * Return: list is not looped - 0
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	tortoise = head->next;
+	hare = (head->next)->next;
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+			return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
 }
